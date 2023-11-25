256137cf31ef181c2b33521299f7b28f40e850a7
Didrik Nathaniel LLoyd Aasland Skjelbred <141577441+DidrikGetacademy@users.noreply.github.com>
2023-11-25 03:02:23 +0100

    Add files via upload

---
+++ OverloadsOppgave.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloads_og_Default_values
+{
+   public class OverloadsOppgave
+   {
+       public string introMessage;
+
+       public int age;
+
+       public string workplace;
+
+
+       public OverloadsOppgave(string IntroMessage)
+       {
+           introMessage = IntroMessage;
+       }
+
+       public OverloadsOppgave(string IntroMessage, int Age)
+       {
+           introMessage = IntroMessage;
+           age = Age;
+       }
+
+       public OverloadsOppgave(string IntroMessage, int Age, string Workplace
+
+       )
+       {
+           introMessage = IntroMessage;
+           age = Age;
+           workplace = Workplace;
+       }
+
+       
+       public void PrintWelcomeMessage (string extrainfo = "Terje")
+       {
+           Console.WriteLine(introMessage + extrainfo);
+        }
+    }
+}
